4fd1b72d7acffde3e8f8cad1775a744c4eb1693c
Jacob Yoon <jacobyoon926@gmail.com>
2018-08-04 22:24:22 -0400

    Edits

--- js/script.js
+++ js/script.js
@@ -1,32 +1,36 @@
 $(document).ready(function(){
 
-	//Fixed Navigation Bar
-	var $navBar = $('.navBar');
-	var $win = $(window);
+	//Parallax On Big Displays
+	if ( $(window).width() > 739) {
+		var $navBar = $('.navBar');
+		var $win = $(window);
+		/*
+		//Fixed Navigation Bar
+		$win.on('scroll', function() {
+			var top = $win.scrollTop();
+			$navBar.css('top', '' + top + 'px');
+			console.log(top);
+		});
+		*/
 
-	$win.on('scroll', function() {
-		var top = $win.scrollTop();
-		$navBar.css('top', '' + top + 'px');
-		console.log(top);
-	});
+		//Parallax Hello
+		var $hello = $('.hello');
 
-	//Parallax Hello
-	var $hello = $('.hello');
+		$win.on('scroll', function() {
+			var top1 = $win.scrollTop()/2;
+			$hello.css('margin-top', '' + top1 + 'px');
+		});
 
-	$win.on('scroll', function() {
-		var top1 = $win.scrollTop()/3;
-		$hello.css('margin-top', '' + top1 + 'px');
-	});
+		//Parallax Skills Box
+		var $skills = $('.skills');
 
-	//Parallax Skills Box
-	var $skills = $('.skills');
-
-	$win.on('scroll', function() {
-		var top = $win.scrollTop()/4;
-		//var vh = $(window).height();
-		//ar past = (top - vh) / 3;
-		$skills.css('margin-top', '' + top + 'px');
-	});
+		$win.on('scroll', function() {
+			var top = $win.scrollTop()/4;
+			//var vh = $(window).height();
+			//ar past = (top - vh) / 3;
+			$skills.css('margin-top', '' + top + 'px');
+		});
+	}
 
 	//Smooth Scrolling Between Links
 	$("a").on('click', function(event) {
@@ -35,7 +39,7 @@ $(document).ready(function(){
 			var hash = this.hash;
 			$('html, body').animate({
 				scrollTop: $(hash).offset().top
-			}, 600, function(){
+			},1000, function(){
 				window.location.hash = hash;
 			});
 		}
@@ -49,8 +53,8 @@ $(document).ready(function(){
 	//Change .scrollbutton position on scroll
 	$(window).on("scroll", function () {
 		if ($(this).scrollTop() > 100) {
-	   		$(".scroll").css("bottom", "-40px");
-			$(".scroll2").css("bottom", "-40px");
+	   		$(".scroll").css("bottom", "-50px");
+			$(".scroll2").css("bottom", "-50px");
 		}
 		else {
 	   		$(".scroll").css("bottom", "20px");
@@ -58,5 +62,17 @@ $(document).ready(function(){
 		}
 	});
 
+	//Disable X Scroll
+	$(function() {
+
+	    var $body = $(document);
+	    $body.bind('scroll', function() {
+	        // "Disable" the horizontal scroll.
+	        if ($body.scrollLeft() !== 0) {
+	            $body.scrollLeft(0);
+	        }
+	    });
+
+	}); 
 
 });
