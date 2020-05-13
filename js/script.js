7dc609f8c7e7da0dc541b2fb58e8a2b6d71c3cf7
Jacob Yoon <jacobyoon926@gmail.com>
2020-05-12 23:16:04 -0400

    New

--- js/script.js
+++ js/script.js
@@ -14,6 +14,8 @@ window.onscroll = function() {
 document.addEventListener('mousemove', function(ev){
     document.getElementById('cursor').style.transform = 'translateY(' + (ev.clientY - 7) + 'px)';
     document.getElementById('cursor').style.transform += 'translateX(' + (ev.clientX - 7) + 'px)';
+    document.getElementById('cursor-behind').style.transform = 'translateY(' + (ev.clientY - 150) + 'px)';
+    document.getElementById('cursor-behind').style.transform += 'translateX(' + (ev.clientX - 150) + 'px)';
 
     //document.getElementById('cursorFollower').style.transform = 'translateY(' + (ev.clientY - 150) + 'px)';
     //document.getElementById('cursorFollower').style.transform += 'translateX(' + (ev.clientX - 150) + 'px)';
@@ -45,3 +47,47 @@ onReady(function () {
     document.getElementById('loader').style.top = "-100vh";
     document.getElementsByTagName('html')[0].style.overflowY = "scroll";
 });
+
+
+/*
+*
+Noise Canvas
+*
+var canvas = document.getElementById('canvas'),
+    ctx = canvas.getContext('2d');
+
+function resize() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+}
+resize();
+window.onresize = resize;
+
+function noise(ctx) {
+    
+    var w = ctx.canvas.width,
+        h = ctx.canvas.height,
+        idata = ctx.createImageData(w, h),
+        buffer32 = new Uint32Array(idata.data.buffer),
+        len = buffer32.length,
+        i = 0;
+
+    for(; i < len;)
+        buffer32[i++] = ((255 * Math.random())|0) << 24;
+    
+    ctx.putImageData(idata, 0, 0);
+}
+
+var toggle = true;
+
+// added toggle to get 30 FPS instead of 60 FPS
+(function loop() {
+    toggle = !toggle;
+    if (toggle) {
+        requestAnimationFrame(loop);
+        return;
+    }
+    noise(ctx);
+    requestAnimationFrame(loop);
+})();
+*/
