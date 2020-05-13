70a69ef0f124cd4148e76817f292049418bb3980
Jacob Yoon <jacobyoon926@gmail.com>
2020-05-13 15:47:10 -0400

    Mobile

---
+++ js/SmoothScroll.js-master/SmoothScroll.js-master/README.md
@@ -0,0 +1,4 @@
+# SmoothScroll.js
+A simple smooth scrolling using 100% vanilla JavaScript.
+
+Live view : https://smooth-scroll-js.netlify.app/
--- js/script.js
+++ js/script.js
@@ -20,3 +20,17 @@ onReady(function () {
     document.getElementById('loader').style.top = "-100vh";
     document.getElementsByTagName('html')[0].style.overflowY = "scroll";
 });
+
+
+
+var offset = 0;
+var halfWidth = window.innerWidth / 2;
+var halfHeight = window.innerHeight / 2;
+var $win = $(window);
+
+document.addEventListener("mousemove", e => {
+    if (window.innerWidth > 1000) {
+        document.getElementById('cursor').style.top = e.pageY - $win.scrollTop() - offset + "px";
+        document.getElementById('cursor').style.left = e.pageX - offset + "px";
+    }
+})
