94793d45a22c2c9e189bed27c911b77878030388
Jacob Yoon <jacobyoon926@gmail.com>
2020-07-01 14:15:34 -0400

    Cards

--- js/script.js
+++ js/script.js
@@ -39,21 +39,66 @@ document.addEventListener("mousemove", e => {
 })
 
 $("a").mouseover(function(){
-    document.getElementById('cursor').style.transform = "scale(5)";
+    document.getElementById('cursor').style.transform = "scale(4)";
 });
 $("a").mouseout(function(){
     document.getElementById('cursor').style.transform = "scale(1)";
 });
+$("button").mouseover(function(){
+    document.getElementById('cursor').style.transform = "scale(4)";
+});
+$("button").mouseout(function(){
+    document.getElementById('cursor').style.transform = "scale(1)";
+});
 
-$(".parent").mouseover(function(){
+/*$(".parent").mouseover(function(){
     document.getElementById('click').style.opacity = "1";
 });
 $(".parent").mouseout(function(){
     document.getElementById('click').style.opacity = "0";
 });
+*/
 
 //Scroll to top
 function topFunction() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+function set(n) {
+    $(".web").css("display", "none");
+    $(".modeling").css("display", "none");
+    $(".gamedev").css("display", "none");
+    $(".illustration").css("display", "none");
+    $(".animation").css("display", "none");
+    if (n == 0) {
+        $(".web").css("display", "inline-block");
+        $(".modeling").css("display", "inline-block");
+        $(".gamedev").css("display", "inline-block");
+        $(".illustration").css("display", "inline-block");
+        $(".animation").css("display", "inline-block");
+    }
+    if (n == 1) {
+        $(".web").css("display", "inline-block");
+    }
+    if (n == 2) {
+        $(".modeling").css("display", "inline-block");
+    }
+    if (n == 3) {
+        $(".gamedev").css("display", "inline-block");
+    }
+    if (n == 4) {
+        $(".illustration").css("display", "inline-block");
+    }
+    if (n == 5) {
+        $(".animation").css("display", "inline-block");
+    }
+    for (i = 0; i < 6; i++) {
+        if (i == n) {
+            document.getElementById("button" + i).classList.add("active");
+        }
+        else {
+            document.getElementById("button" + i).classList.remove("active");
+        }
+    }    
 }
