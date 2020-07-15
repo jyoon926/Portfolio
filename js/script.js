da4c1fd6df4e0467b8c00ea364917e674b5a23a1
Jacob Yoon <jacobyoon926@gmail.com>
2020-07-15 12:29:29 -0400

    Update

--- js/script.js
+++ js/script.js
@@ -1,31 +1,3 @@
-//Loader
-/*
-window.onload = function load() {
-}
-
-function onReady(callback) {
-    var intervalID = window.setInterval(checkReady, 1000);
-
-    function checkReady() {
-        if (document.getElementsByTagName('body')[0] !== undefined) {
-            window.clearInterval(intervalID);
-            callback.call(this);
-        }
-    }
-}
-
-function show(id, value) {
-    document.getElementById(id).style.display = value ? 'block' : 'none';
-}
-
-onReady(function () {
-    document.getElementById('loader').style.opacity = "0";
-    document.getElementById('loader').style.pointerEvents = "none";
-    document.getElementById('loading').style.opacity = "0";
-    document.getElementsByTagName('html')[0].style.overflowY = "scroll";
-});
-*/
-
 //Mouse Cursor
 var offset = 10;
 var $win = $(window);
@@ -200,3 +172,42 @@ function set(n) {
         }
     }    
 }
+
+//Noise Canvas
+var canvas = document.getElementById('canvas'),
+    ctx = canvas.getContext('2d');
+
+function resize() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+}
+resize();
+window.onresize = resize;
+
+function noise(ctx) {
+    
+    var w = ctx.canvas.width,
+        h = ctx.canvas.height,
+        idata = ctx.createImageData(w, h),
+        buffer32 = new Uint32Array(idata.data.buffer),
+        len = buffer32.length,
+        i = 0;
+
+    for(; i < len;)
+        buffer32[i++] = ((255 * Math.random())|0) << 24;
+    
+    ctx.putImageData(idata, 0, 0);
+}
+
+var toggle = true;
+
+// added toggle to get 30 FPS instead of 60 FPS
+(function loop() {
+    toggle = !toggle;
+    if (toggle) {
+        requestAnimationFrame(loop);
+        return;
+    }
+    noise(ctx);
+    requestAnimationFrame(loop);
+})();
