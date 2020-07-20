3d3aa05b5ecca8c62676d2eff7fb3d1c8463e6e7
Jacob Yoon <jacobyoon926@gmail.com>
2020-07-20 13:40:15 -0400

    Mobile

--- js/script.js
+++ js/script.js
@@ -21,6 +21,16 @@ $("button").mouseover(function(){
 $("button").mouseout(function(){
     document.getElementById('cursor').style.transform = "scale(1)";
 });
+$(".parent").mouseover(function(){
+    document.getElementById('cursor').style.transform = "scale(7)";
+    document.getElementById('cursor').style.background = "#111112";
+    document.getElementById('cursortext').style.opacity = "1";
+})
+$(".parent").mouseout(function(){
+    document.getElementById('cursor').style.transform = "scale(1)";
+    document.getElementById('cursor').style.background = "none";
+    document.getElementById('cursortext').style.opacity = "0";
+});
 
 //Filter Buttons
 function set(n) {
@@ -103,3 +113,27 @@ function noise() {
     setTimeout(noise, 50);
 }
 noise();
+
+//Mobile thumbnail morph
+function scroll() {
+    if (window.innerWidth < 800) {
+        var elements = document.getElementsByClassName("thumbnail");
+    
+        for(var i = 0; i < elements.length; i++)
+        {
+            var elementPosition = elements[i].getBoundingClientRect();
+            var pos = elementPosition.y + (elements[i].offsetHeight / 2);
+            var frac = 1.25 - (Math.abs(window.innerHeight / 2 - pos) / (window.innerHeight / 2));
+            var other = 1 - (Math.abs(window.innerHeight / 2 - pos) / (window.innerHeight / 2));
+            if (frac < 0)
+                frac = 0;
+            else if (frac > 1)
+                frac = 1;
+            if (other < 0)
+                other = 0;
+            else if (other > 1)
+                other = 1;
+            elements[i].style.filter = "brightness(" + frac + ") saturate(" + other + ")";
+        }
+    }
+}
