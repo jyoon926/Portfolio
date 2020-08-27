d8d15861395b42d2c28ff22710288d24cb894839
Jacob Yoon <jacobyoon926@gmail.com>
2020-08-27 18:12:36 -0400

    Only websites

---
+++ Complete Portfolio/js/script.js
@@ -0,0 +1,136 @@
+//Mouse Cursor
+var offset = 10;
+var $win = $(window);
+
+document.addEventListener("mousemove", e => {
+    if (window.innerWidth > 1000) {
+        document.getElementById('cursor').style.top = parseInt(e.pageY, 10) - $win.scrollTop() - offset + "px";
+        document.getElementById('cursor').style.left = parseInt(e.pageX, 10) - offset + "px";
+    }
+})
+
+$("a").mouseover(function(){
+    document.getElementById('cursor').style.transform = "scale(5)";
+});
+$("a").mouseout(function(){
+    document.getElementById('cursor').style.transform = "scale(1)";
+});
+$("button").mouseover(function(){
+    document.getElementById('cursor').style.transform = "scale(5)";
+});
+$("button").mouseout(function(){
+    document.getElementById('cursor').style.transform = "scale(1)";
+});
+$(".parent").mouseover(function(){
+    document.getElementById('cursor').style.transform = "scale(7)";
+    document.getElementById('cursor').style.background = "#111112";
+    document.getElementById('cursortext').style.opacity = "1";
+})
+$(".parent").mouseout(function(){
+    document.getElementById('cursor').style.transform = "scale(1)";
+    document.getElementById('cursor').style.background = "none";
+    document.getElementById('cursortext').style.opacity = "0";
+});
+
+//Filter Buttons
+function set(n) {
+    $(".web").css("display", "none");
+    $(".modeling").css("display", "none");
+    $(".gamedev").css("display", "none");
+    $(".illustration").css("display", "none");
+    $(".animation").css("display", "none");
+    if (n == 0) {
+        $(".web").css("display", "flex");
+        $(".modeling").css("display", "flex");
+        $(".gamedev").css("display", "flex");
+        $(".illustration").css("display", "flex");
+        $(".animation").css("display", "flex");
+    }
+    if (n == 1) {
+        $(".web").css("display", "flex");
+    }
+    if (n == 2) {
+        $(".modeling").css("display", "flex");
+    }
+    if (n == 3) {
+        $(".gamedev").css("display", "flex");
+    }
+    if (n == 4) {
+        $(".illustration").css("display", "flex");
+    }
+    if (n == 5) {
+        $(".animation").css("display", "flex");
+    }
+    for (i = 0; i < 6; i++) {
+        if (i == n) {
+            document.getElementById("button" + i).classList.add("active");
+        }
+        else {
+            document.getElementById("button" + i).classList.remove("active");
+        }
+    }    
+}
+
+//Scroll Down Link
+function about() {
+    var elmnt = document.getElementById("anchor");
+    elmnt.scrollIntoView();
+}
+
+//Noise Canvas
+var canvas = document.getElementById('canvas'),
+    ctx = canvas.getContext('2d');
+
+function resize() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+}
+resize();
+window.onresize = resize;
+
+function noise() {
+    
+    var w = ctx.canvas.width,
+        h = ctx.canvas.height,
+        idata = ctx.createImageData(w, h),
+        buffer32 = new Uint32Array(idata.data.buffer),
+        len = buffer32.length,
+        i = 0;
+
+    for(; i < len;)
+        buffer32[i++] = ((255 * Math.random())|0) << 24;
+    
+    ctx.putImageData(idata, 0, 0);
+
+    setTimeout(noise, 50);
+}
+noise();
+
+//Mobile thumbnail morph
+function scroll() {
+    var flowers = document.getElementsByClassName("flower");
+    for (var i = 0; i < 2; i++) {
+        flowers[i].style.transform = "rotate(" + flowers[i].getBoundingClientRect().top / -15 + "deg)";
+    }
+    
+    if (window.innerWidth < 800) {
+        var elements = document.getElementsByClassName("thumbnail");
+    
+        for(var i = 0; i < elements.length; i++)
+        {
+            var elementPosition = elements[i].getBoundingClientRect();
+            var pos = elementPosition.y + (elements[i].offsetHeight / 2);
+            var frac = 1.25 - (Math.abs(window.innerHeight / 2 - pos) / (window.innerHeight / 2));
+            var other = 1 - (Math.abs(window.innerHeight / 2 - pos) / (window.innerHeight / 2));
+            if (frac < 0)
+                frac = 0;
+            else if (frac > 1)
+                frac = 1;
+            if (other < 0)
+                other = 0;
+            else if (other > 1)
+                other = 1;
+            elements[i].style.filter = "brightness(" + frac + ") saturate(" + other + ")";
+        }
+    }
+}
--- js/script.js
+++ js/script.js
@@ -1,136 +1,75 @@
-//Mouse Cursor
-var offset = 10;
-var $win = $(window);
+var index = 0;
+var count = 7;
+var delay = 500;
+var animating = false;
 
-document.addEventListener("mousemove", e => {
-    if (window.innerWidth > 1000) {
-        document.getElementById('cursor').style.top = parseInt(e.pageY, 10) - $win.scrollTop() - offset + "px";
-        document.getElementById('cursor').style.left = parseInt(e.pageX, 10) - offset + "px";
+for (var i = 0; i < 7; i++) {
+    if (i == 0) {
+        document.getElementById("section" + i).style.top ="0";
     }
-})
+    else {
+        document.getElementById("section" + i).style.top ="100vh";
+        document.getElementById("divider" + i).style.top ="100vh";
+    }
+}
 
-$("a").mouseover(function(){
-    document.getElementById('cursor').style.transform = "scale(5)";
-});
-$("a").mouseout(function(){
-    document.getElementById('cursor').style.transform = "scale(1)";
-});
-$("button").mouseover(function(){
-    document.getElementById('cursor').style.transform = "scale(5)";
-});
-$("button").mouseout(function(){
-    document.getElementById('cursor').style.transform = "scale(1)";
-});
-$(".parent").mouseover(function(){
-    document.getElementById('cursor').style.transform = "scale(7)";
-    document.getElementById('cursor').style.background = "#111112";
-    document.getElementById('cursortext').style.opacity = "1";
-})
-$(".parent").mouseout(function(){
-    document.getElementById('cursor').style.transform = "scale(1)";
-    document.getElementById('cursor').style.background = "none";
-    document.getElementById('cursortext').style.opacity = "0";
-});
+function setIn(num) {
+    document.getElementById("divider" + num).style.top ="0";
+    setTimeout(function(){
+        document.getElementById("section" + num).style.top ="0";
+    }, delay);
+}
 
-//Filter Buttons
-function set(n) {
-    $(".web").css("display", "none");
-    $(".modeling").css("display", "none");
-    $(".gamedev").css("display", "none");
-    $(".illustration").css("display", "none");
-    $(".animation").css("display", "none");
-    if (n == 0) {
-        $(".web").css("display", "flex");
-        $(".modeling").css("display", "flex");
-        $(".gamedev").css("display", "flex");
-        $(".illustration").css("display", "flex");
-        $(".animation").css("display", "flex");
-    }
-    if (n == 1) {
-        $(".web").css("display", "flex");
-    }
-    if (n == 2) {
-        $(".modeling").css("display", "flex");
-    }
-    if (n == 3) {
-        $(".gamedev").css("display", "flex");
-    }
-    if (n == 4) {
-        $(".illustration").css("display", "flex");
-    }
-    if (n == 5) {
-        $(".animation").css("display", "flex");
-    }
-    for (i = 0; i < 6; i++) {
-        if (i == n) {
-            document.getElementById("button" + i).classList.add("active");
+function setOut(num) {
+    document.getElementById("section" + num).style.top ="100vh";
+    setTimeout(function(){
+        document.getElementById("divider" + num).style.top ="100vh";
+    }, delay);
+}
+
+$(function() {
+    $("#container").bind("mousewheel", function(e) {
+        if (!animating) {
+            var temp = index;
+            //back
+            if (e.originalEvent.wheelDelta > 0) {
+                if (index > 0) {
+                    setOut(index);
+                    index--;
+                    animating = true;
+                    setTimeout(function(){
+                        animating = false;
+                    }, 800);
+                }
+            }
+            //next
+            else {
+                if (index < count - 1) {
+                    index++;
+                    setIn(index);
+                    animating = true;
+                    setTimeout(function(){
+                        animating = false;
+                    }, 800);
+                }
+            }
+            document.getElementById("scrollbar").style.height = (index * (100 / 6)) + "vh";
+            return false;
         }
-        else {
-            document.getElementById("button" + i).classList.remove("active");
-        }
-    }    
-}
+    });
+});
 
-//Scroll Down Link
-function about() {
-    var elmnt = document.getElementById("anchor");
-    elmnt.scrollIntoView();
-}
-
-//Noise Canvas
-var canvas = document.getElementById('canvas'),
-    ctx = canvas.getContext('2d');
-
-function resize() {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-}
-resize();
-window.onresize = resize;
-
-function noise() {
-    
-    var w = ctx.canvas.width,
-        h = ctx.canvas.height,
-        idata = ctx.createImageData(w, h),
-        buffer32 = new Uint32Array(idata.data.buffer),
-        len = buffer32.length,
-        i = 0;
-
-    for(; i < len;)
-        buffer32[i++] = ((255 * Math.random())|0) << 24;
-    
-    ctx.putImageData(idata, 0, 0);
-
-    setTimeout(noise, 50);
-}
-noise();
-
-//Mobile thumbnail morph
-function scroll() {
-    var flowers = document.getElementsByClassName("flower");
-    for (var i = 0; i < 2; i++) {
-        flowers[i].style.transform = "rotate(" + flowers[i].getBoundingClientRect().top / -15 + "deg)";
-    }
-    
-    if (window.innerWidth < 800) {
-        var elements = document.getElementsByClassName("thumbnail");
-    
-        for(var i = 0; i < elements.length; i++)
-        {
-            var elementPosition = elements[i].getBoundingClientRect();
-            var pos = elementPosition.y + (elements[i].offsetHeight / 2);
-            var frac = 1.25 - (Math.abs(window.innerHeight / 2 - pos) / (window.innerHeight / 2));
-            var other = 1 - (Math.abs(window.innerHeight / 2 - pos) / (window.innerHeight / 2));
-            if (frac < 0)
-                frac = 0;
-            else if (frac > 1)
-                frac = 1;
-            if (other < 0)
-                other = 0;
-            else if (other > 1)
-                other = 1;
-            elements[i].style.filter = "brightness(" + frac + ") saturate(" + other + ")";
-        }
-    }
-}
+//Loader
+$(window).load(function() {
+    setTimeout(function(){
+        document.getElementById('loadertext').style.transitionDuration = "1s";
+        document.getElementById('loadertext').style.opacity = "0";
+        document.getElementById('loader').style.transitionDelay = "1s";
+        document.getElementById('loader').style.transitionDuration = "1s";
+        document.getElementById('loader').style.opacity = "0";
+    }, 100);
+    setTimeout(function(){
+        document.getElementById('loader').style.pointerEvents = "none";
+        document.getElementsByTagName('html')[0].style.overflowY = "scroll";
+    }, 1000);
+});
