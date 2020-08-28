bbc3f2320a1db57e8976ab43b3765cc6a8885f50
Jacob Yoon <jacobyoon926@gmail.com>
2020-08-27 20:28:48 -0400

    Update

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 var index = 0;
-var count = 7;
+var count = 8;
 var delay = 500;
 var animating = false;
 
-for (var i = 0; i < 7; i++) {
+for (var i = 0; i < count; i++) {
     if (i == 0) {
         document.getElementById("section" + i).style.top ="0";
     }
@@ -13,30 +13,50 @@ for (var i = 0; i < 7; i++) {
     }
 }
 
-function setIn(num) {
-    document.getElementById("divider" + num).style.top ="0";
-    setTimeout(function(){
-        document.getElementById("section" + num).style.top ="0";
-    }, delay);
-}
-
-function setOut(num) {
-    document.getElementById("section" + num).style.top ="100vh";
-    setTimeout(function(){
-        document.getElementById("divider" + num).style.top ="100vh";
-    }, delay);
+function set(num) {
+    document.getElementById("checkbox").checked = false;
+    if (num > index) {
+        for (var i = 0; i < count; i++) {
+            if (i <= num) {
+                if (i != 0) {
+                    document.getElementById("divider" + i).style.top ="0";
+                }
+            }
+            else {
+                document.getElementById("divider" + i).style.top ="100vh";
+                document.getElementById("section" + i).style.top ="100vh";
+            }
+        }
+        setTimeout(function(){
+            document.getElementById("section" + num).style.top ="0";
+        }, delay);
+    }
+    else if (num < index) {
+        for (var i = 0; i < count; i++) {
+            if (i > num) {
+                document.getElementById("section" + i).style.top ="100vh";
+                if (i != 7)
+                    document.getElementById("divider" + (i + 1)).style.top ="100vh";
+            }
+            else {
+                document.getElementById("section" + i).style.top ="0";
+            }
+        }
+        setTimeout(function(){
+            document.getElementById("divider" + (num + 1)).style.top ="100vh";
+        }, delay);
+    }
+    index = num;
 }
 
 $(function() {
     $("#container").on("wheel ", function(e) {
-        console.log("scroll");
         if (!animating) {
             var temp = index;
             //back
             if (e.originalEvent.deltaY < 0) {
                 if (index > 0) {
-                    setOut(index);
-                    index--;
+                    set(index - 1);
                     animating = true;
                     setTimeout(function(){
                         animating = false;
@@ -46,15 +66,15 @@ $(function() {
             //next
             else {
                 if (index < count - 1) {
-                    index++;
-                    setIn(index);
+                    set(index + 1);
                     animating = true;
                     setTimeout(function(){
                         animating = false;
                     }, 800);
                 }
             }
-            document.getElementById("scrollbar").style.height = (index * (100 / 6)) + "vh";
+            document.getElementById("scrollbar").style.height = (index * (100 / (count - 1))) + "vh";
+            console.log(index);
             return false;
         }
     });
