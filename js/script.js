3bbcfa6d5794a75e5ab8bfd63d885125c77e7daa
Jacob Yoon <jacobyoon926@gmail.com>
2020-09-04 11:45:22 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ var animating = false;
 
 function start(num) {
     count = num;
+    setGradients();
 }
 
 for (var i = 0; i < count; i++) {
@@ -18,18 +19,20 @@ for (var i = 0; i < count; i++) {
 }
 
 var slides = document.getElementsByClassName("right");
-var right = "linear-gradient(to right, rgba(0, 0, 0, 0) 70%, rgba(0, 0, 0, 0.75))";
+var right = "linear-gradient(to right, rgba(0, 0, 0, 0) 70%, rgba(0, 0, 0, 0.5))";
 var bottom = "linear-gradient(rgba(0, 0, 0, 0) 80%, rgba(0, 0, 0, 0.5))";
-for (var i = 1; i < slides.length; i++) {
-    var item = slides.item(i);
-    var image = String(getComputedStyle(item).backgroundImage);
-    if (item.offsetWidth / item.offsetHeight > 16 / 9) {
-        item.style.backgroundImage = bottom + ", " + image;
+function setGradients() {
+    for (var i = 1; i < slides.length; i++) {
+        var item = slides.item(i);
+        var image = String(getComputedStyle(item).backgroundImage);
+        if (item.offsetWidth / item.offsetHeight > 16 / 9) {
+            item.style.backgroundImage = bottom + ", " + image;
+        }
+        else {
+            item.style.backgroundImage = right + ", " + image;
+        }
+        item.style.backgroundSize = "cover";
     }
-    else {
-        item.style.backgroundImage = right + ", " + image;
-    }
-    item.style.backgroundSize = "cover";
 }
 function resize() {
     for (var i = 1; i < slides.length; i++) {
@@ -37,8 +40,8 @@ function resize() {
         var image = String(getComputedStyle(item).backgroundImage);
         if (item.offsetWidth / item.offsetHeight > 16 / 9) {
             if (String(getComputedStyle(item).backgroundImage).charAt(16) == "t") {
-                console.log(bottom + ", " + image.substring(70));
-                item.style.backgroundImage = bottom + ", " + image.substring(70);
+                console.log(bottom + ", " + image.substring(69));
+                item.style.backgroundImage = bottom + ", " + image.substring(69);
             }
         }
         else {
@@ -150,4 +153,70 @@ document.addEventListener("mousemove", e => {
     }
 })
 
-//Firefox
+//Swipe
+
+document.addEventListener('touchstart', handleTouchStart, false);        
+document.addEventListener('touchmove', handleTouchMove, false);
+
+var xDown = null;                                                        
+var yDown = null;
+
+function getTouches(evt) {
+  return evt.touches ||             // browser API
+         evt.originalEvent.touches; // jQuery
+}                                                     
+
+function handleTouchStart(evt) {
+    const firstTouch = getTouches(evt)[0];                                      
+    xDown = firstTouch.clientX;                                      
+    yDown = firstTouch.clientY;                                      
+};                                                
+
+function handleTouchMove(evt) {
+    if ( ! xDown || ! yDown ) {
+        return;
+    }
+
+    var xUp = evt.touches[0].clientX;                                    
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+        if ( xDiff > 0 ) {
+            /* left swipe */ 
+        } else {
+            /* right swipe */
+        }                       
+    } else {
+        if (!animating) {
+            var temp = index;
+            if ( yDiff > 0 ) {
+                if (index < count - 1) {
+                    set(index + 1);
+                    animating = true;
+                    setTimeout(function(){
+                        animating = false;
+                    }, 800);
+                }
+            } else { 
+                if (index > 0) {
+                    set(index - 1);
+                    animating = true;
+                    setTimeout(function(){
+                        animating = false;
+                    }, 800);
+                }
+            }    
+            document.getElementById("scrollbar").style.height = (index * (100 / (count - 1))) + "vh";
+            console.log(index);
+        }
+        else {
+            return;
+        }                                                             
+    }
+    /* reset values */
+    xDown = null;
+    yDown = null;                                             
+};
