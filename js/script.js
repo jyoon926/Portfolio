bf0239c9fb1d5333cc6258d3fd7f33f8c52ce005
Jacob Yoon <jacobyoon926@gmail.com>
2020-09-11 20:39:18 -0400

    new again bruh

--- js/script.js
+++ js/script.js
@@ -1,3 +1,146 @@
+//Loader
+$(window).load(function() {
+    setTimeout(function(){
+        document.getElementById('loadertext').style.transitionDuration = "1s";
+        document.getElementById('loadertext').style.opacity = "0";
+        document.getElementById('loader').style.transitionDelay = "1s";
+        document.getElementById('loader').style.transitionDuration = "1s";
+        document.getElementById('loader').style.opacity = "0";
+    }, 100);
+    setTimeout(function(){
+        document.getElementById('loader').style.pointerEvents = "none";
+        document.getElementsByTagName('html')[0].style.overflowY = "scroll";
+    }, 1000);
+});
+
+//Cursor
+var offset = 9;
+var offsetF = 40;
+var $win = $(window);
+
+document.addEventListener("mousemove", e => {
+    if (window.innerWidth > 850) {
+        document.getElementById('cursor').style.top = e.pageY - offset + "px";
+        document.getElementById('cursor').style.left = e.pageX - offset + "px";
+        // document.getElementById('cursorF').style.top = e.pageY - offsetF + "px";
+        // document.getElementById('cursorF').style.left = e.pageX - offsetF + "px";
+    }
+});
+$('a').mouseenter(
+    function() {
+        document.getElementById('cursor').style.transform = "scale(5)";
+    }
+);
+$('a').mouseout(
+    function() {
+        document.getElementById('cursor').style.transform = "scale(1)";
+    }
+);
+function hoverIn(i) {
+    if (i == "left" && index > 1)
+        document.getElementById('cursor').style.transform = "scale(5)";
+    else if (i == "right" && index < 4)
+        document.getElementById('cursor').style.transform = "scale(5)";
+    else
+        document.getElementById('cursor').style.transform = "scale(1)";
+}
+function hoverOut() {
+    document.getElementById('cursor').style.transform = "scale(1)";
+}
+
+//Home slideshow
+var index = 1;
+function next() {
+    if (index == 1) {
+        $('.leftarrow').css("opacity", "1");
+    }
+    else if (index == 3) {
+        $('.rightarrow').css("opacity", "0.3");
+    }
+    if (index < 4)
+        index++;
+    document.getElementById("showcase").style.transform = "translateX(-" + 100 * (index - 1) + "vw)";
+}
+function previous() {
+    if (index == 4) {
+        $('.rightarrow').css("opacity", "1");
+    }
+    else if (index == 2) {
+        $('.leftarrow').css("opacity", "0.3");
+    }
+    if (index > 1)
+        index--;
+    document.getElementById("showcase").style.transform = "translateX(-" + 100 * (index - 1) + "vw)";
+}
+
+/**
+//Swipe
+
+document.addEventListener('touchstart', handleTouchStart, false);        
+document.addEventListener('touchmove', handleTouchMove, false);
+
+var xDown = null;                                                        
+var yDown = null;
+
+function getTouches(evt) {
+  return evt.touches ||             // browser API
+         evt.originalEvent.touches; // jQuery
+}                                                     
+
+function handleTouchStart(evt) {
+    const firstTouch = getTouches(evt)[0];                                      
+    xDown = firstTouch.clientX;                                      
+    yDown = firstTouch.clientY;                                      
+};                                                
+
+function handleTouchMove(evt) {
+    if ( ! xDown || ! yDown ) {
+        return;
+    }
+
+    var xUp = evt.touches[0].clientX;                                    
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {
+        if ( xDiff > 0 ) {
+        } else {
+        }                       
+    } else {
+        if (!animating) {
+            var temp = index;
+            if ( yDiff > 0 ) {
+                if (index < count - 1) {
+                    set(index + 1);
+                    animating = true;
+                    setTimeout(function(){
+                        animating = false;
+                    }, 800);
+                }
+            } else { 
+                if (index > 0) {
+                    set(index - 1);
+                    animating = true;
+                    setTimeout(function(){
+                        animating = false;
+                    }, 800);
+                }
+            }    
+            document.getElementById("scrollbar").style.height = (index * (100 / (count - 1))) + "vh";
+            console.log(index);
+        }
+        else {
+            return;
+        }                                                             
+    }
+    xDown = null;
+    yDown = null;                                             
+};
+
+*/
+/**
 var index = 0;
 var count;
 var delay = 500;
@@ -22,35 +165,58 @@ var slides = document.getElementsByClassName("right");
 var right = "linear-gradient(to right, rgba(0, 0, 0, 0) 70%, rgba(0, 0, 0, 0.5))";
 var bottom = "linear-gradient(rgba(0, 0, 0, 0) 80%, rgba(0, 0, 0, 0.5))";
 function setGradients() {
-    for (var i = 1; i < slides.length; i++) {
-        var item = slides.item(i);
-        var image = String(getComputedStyle(item).backgroundImage);
-        if (item.offsetWidth / item.offsetHeight > 16 / 9) {
-            item.style.backgroundImage = bottom + ", " + image;
+    if (window.innerWidth > 700) {
+        for (var i = 1; i < slides.length; i++) {
+            var item = slides.item(i);
+            var image = String(getComputedStyle(item).backgroundImage);
+            if (item.offsetWidth / item.offsetHeight > 16 / 9) {
+                item.style.backgroundImage = bottom + ", " + image;
+            }
+            else {
+                item.style.backgroundImage = right + ", " + image;
+            }
+            item.style.backgroundSize = "cover";
         }
-        else {
-            item.style.backgroundImage = right + ", " + image;
+    }
+    else {
+        for (var i = 1; i < slides.length; i++) {
+            var item = slides.item(i);
+            item.style.height = (window.innerWidth - 60) * (9 / 16) + "px";
         }
-        item.style.backgroundSize = "cover";
     }
 }
 function resize() {
-    for (var i = 1; i < slides.length; i++) {
-        var item = slides.item(i);
-        var image = String(getComputedStyle(item).backgroundImage);
-        if (item.offsetWidth / item.offsetHeight > 16 / 9) {
-            if (String(getComputedStyle(item).backgroundImage).charAt(16) == "t") {
-                console.log(bottom + ", " + image.substring(69));
-                item.style.backgroundImage = bottom + ", " + image.substring(69);
+    if (window.innerWidth > 700) {
+        for (var i = 1; i < slides.length; i++) {
+            var item = slides.item(i);
+            var image = String(getComputedStyle(item).backgroundImage);
+            if (item.offsetWidth / item.offsetHeight > 16 / 9) {
+                if (String(getComputedStyle(item).backgroundImage).charAt(16) == "t") {
+                    item.style.backgroundImage = bottom + ", " + image.substring(69);
+                }
             }
-        }
-        else {
-            if (String(getComputedStyle(item).backgroundImage).charAt(16) == "r") {
-                console.log(right + ", " + image.substring(59));
-                item.style.backgroundImage = right + ", " + image.substring(59);
+            else {
+                if (String(getComputedStyle(item).backgroundImage).charAt(16) == "r") {
+                    item.style.backgroundImage = right + ", " + image.substring(59);
+                }
             }
+            item.style.backgroundSize = "cover";
         }
-        item.style.backgroundSize = "cover";
+    }
+    else {
+        for (var i = 0; i < slides.length; i++) {
+            var item = slides.item(i);
+            item.style.height = (window.innerWidth - 60) * (9 / 16) + "px";
+        }
+    }
+    var rotate = document.getElementById("rotate");
+    if (rotate.offsetWidth < 1000 && rotate.offsetHeight < 450) {
+        rotate.style.opacity = "1";
+        rotate.style.pointerEvents = "auto";
+    }
+    else {
+        rotate.style.opacity = "0";
+        rotate.style.pointerEvents = "none";
     }
 }
 
@@ -123,100 +289,4 @@ $(function() {
         }
     });
 });
-
-//Loader
-$(window).load(function() {
-    setTimeout(function(){
-        document.getElementById('loadertext').style.transitionDuration = "1s";
-        document.getElementById('loadertext').style.opacity = "0";
-        document.getElementById('loader').style.transitionDelay = "1s";
-        document.getElementById('loader').style.transitionDuration = "1s";
-        document.getElementById('loader').style.opacity = "0";
-    }, 100);
-    setTimeout(function(){
-        document.getElementById('loader').style.pointerEvents = "none";
-        document.getElementsByTagName('html')[0].style.overflowY = "scroll";
-    }, 1000);
-});
-
-//Cursor
-var offset = 9;
-var offsetF = 40;
-var $win = $(window);
-
-document.addEventListener("mousemove", e => {
-    if (window.innerWidth > 1000) {
-        document.getElementById('cursor').style.top = e.pageY - offset + "px";
-        document.getElementById('cursor').style.left = e.pageX - offset + "px";
-        document.getElementById('cursorF').style.top = e.pageY - offsetF + "px";
-        document.getElementById('cursorF').style.left = e.pageX - offsetF + "px";
-    }
-})
-
-//Swipe
-
-document.addEventListener('touchstart', handleTouchStart, false);        
-document.addEventListener('touchmove', handleTouchMove, false);
-
-var xDown = null;                                                        
-var yDown = null;
-
-function getTouches(evt) {
-  return evt.touches ||             // browser API
-         evt.originalEvent.touches; // jQuery
-}                                                     
-
-function handleTouchStart(evt) {
-    const firstTouch = getTouches(evt)[0];                                      
-    xDown = firstTouch.clientX;                                      
-    yDown = firstTouch.clientY;                                      
-};                                                
-
-function handleTouchMove(evt) {
-    if ( ! xDown || ! yDown ) {
-        return;
-    }
-
-    var xUp = evt.touches[0].clientX;                                    
-    var yUp = evt.touches[0].clientY;
-
-    var xDiff = xDown - xUp;
-    var yDiff = yDown - yUp;
-
-    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
-        if ( xDiff > 0 ) {
-            /* left swipe */ 
-        } else {
-            /* right swipe */
-        }                       
-    } else {
-        if (!animating) {
-            var temp = index;
-            if ( yDiff > 0 ) {
-                if (index < count - 1) {
-                    set(index + 1);
-                    animating = true;
-                    setTimeout(function(){
-                        animating = false;
-                    }, 800);
-                }
-            } else { 
-                if (index > 0) {
-                    set(index - 1);
-                    animating = true;
-                    setTimeout(function(){
-                        animating = false;
-                    }, 800);
-                }
-            }    
-            document.getElementById("scrollbar").style.height = (index * (100 / (count - 1))) + "vh";
-            console.log(index);
-        }
-        else {
-            return;
-        }                                                             
-    }
-    /* reset values */
-    xDown = null;
-    yDown = null;                                             
-};
+*/
